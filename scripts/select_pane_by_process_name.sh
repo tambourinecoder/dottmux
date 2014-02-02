07783a698b88b54fd51556072274ba59be4ddb68
Tambourinecoder <tambourinecoder@gmail.com>
2014-02-02 21:06:34 +0100

    Select pane by process name

---
+++ scripts/select_pane_by_process_name.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+function psSubtree {
+  echo `pstree $1 | tr '[:upper:]' '[:lower:]'`
+}
+
+function selectPaneByProcessName {
+  for paneInfo in `tmux list-panes -F '#{pane_pid}-#{window_index}.#{pane_index}'` ; do
+    panePid=`echo $paneInfo | cut -d '-' -f1`
+    windowAndPane=`echo $paneInfo | cut -d '-' -f2`
+
+    if [[ `psSubtree $panePid` =~ $1 ]]; then
+      tmux select-pane -t $windowAndPane
+    fi
+  done
+}
+
+selectPaneByProcessName $1
